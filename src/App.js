209728985d69592e22406ad0dafea50747d5d191
Okano Takumi <oknokn.of.12@gmail.com>
2020-05-11 16:43:02 +0900

    make app and learn React Hooks with useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+//Hooksの学習
+const App = () => {
+  //第1戻り値には値(状態)、2つ目は関数が帰ってくる。(set~が慣習)
+  const [count,setCount] = useState(0)
+  // console.log(count);
+  // console.log(setCount);
+
+  //setCountに値や関数を渡すことができる。
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const incrementFunc = () => setCount(previousCount => {
+    if (previousCount > 0) {
+      return 10;
+    } 
+    else {
+      return 0;
+    }
+  })
+  const decrementFunc = () => setCount(previousCount => previousCount - 1)
+
+  const reset = () => setCount(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<React.Flagment>は<>で省略できる
+    <>
+      <div>
+          count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={incrementFunc}>Plus</button>
+        <button onClick={decrementFunc}>Minus</button>
+      </div>
+      <button onClick={reset}>Reset</button>
+    </>
   );
 }
 
